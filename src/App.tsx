e08e26744ee41b90e52e18326bdd527edcd4456b
Mikael Hallendal <hallski@hallski.org>
2019-10-26 23:53:30 +0200

    Remove semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react"
+import "./App.css"
+import logo from "./logo.svg"
 
-const Button = ({title}: {title: string}) =>
-    <button>{title}</button>
+const Button = ({ title }: { title: string }) => <button>{title}</button>
 
-const Title = ({label}: {label: string}) =>
-    <h1>{label}</h1>
+const Title = ({
+  label,
+  description
+}: {
+  label: string
+  description: string
+}) => <h1>{label}</h1>
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
 
-        <Button    label="oaisjdo"/>
+      <Button title="Sliff" />
+      <Title label="Foo" description="" />
 
-        <Button title="Sliff" />
+      <Title label="Oh yeah" description="" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+)
 
-        <Title label="Oh yeah" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MyComponent: React.FC = () => <div>Hello there</div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <MyComponent>
+    <App />
+  </MyComponent>,
+  document.getElementById("root")
+)
