f9d4f8afe72434942a19ef25e114c1e1f92bd8b5
Mikael Hallendal <hallski@hallski.org>
2019-11-04 23:38:09 +0100

    Update test with testable effects

--- src/app-state.test.ts
+++ src/app-state.test.ts
@@ -1,14 +1,13 @@
-import { update, initialState, fetchTodos } from "./app-state"
+import { update, initialState } from "./app-state"
 
 it("Should run a timeout on Add", () => {
   const { state, effect } = update(initialState, { type: "Add", x: 10, y: 20 })
   expect(state.result).toBe(30)
-  expect(effect!.name).toEqual("promiseTimeout")
-  expect(effect!.data).toEqual({ duration: 2000 })
+  expect(effect!).toEqual({ type: "Timeout", duration: 2000 })
 })
 
 it("Should request todos after timeout", () => {
   const { state, effect } = update(initialState, { type: "AfterTimeout" })
   expect(state.timeoutDone).toBe(true)
-  expect(effect).toEqual(fetchTodos)
+  expect(effect).toEqual({ type: "FetchTodos" })
 })
